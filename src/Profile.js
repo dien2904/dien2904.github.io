7c9920e1bee7a8f4d17a2e69febe4b84f2ec6605
dien2904 <havandien2004@gmail.com>
2024-03-07 13:00:05 +0700

    first commit

---
+++ src/Profile.js
@@ -0,0 +1,57 @@
+function Profile() {
+    return (
+      <div className="Profile">
+        <div className="nav"></div>
+        <div className="row-1">
+          <div className="col1">
+            <div className="image"> <img src="https://scontent.fbmv1-1.fna.fbcdn.net/v/t39.30808-6/325473752_1003470023959888_7610007276324676492_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=sHkMguXqeIkAX8c8Awf&_nc_ht=scontent.fbmv1-1.fna&oh=00_AfAmOddOw3Dy_JchDkrPJHVbo_YY1D625aJFvkiOKz4NcA&oe=65ED522B" ></img></div>
+          </div>
+          <div className="sline"></div>
+          <div className="col2">
+            <div className="name">Hà Văn Diện</div>
+            <div className="information">Apprentice Programmer</div>
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="row-2">
+          <div className="col-1">
+            <div className="title">contact</div>
+            <div className="infor"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg> 0917515947</div>
+            <div className="infor"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+</svg> Hà Văn Diện</div>
+            <div className="infor"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
+</svg> havandien2004@gmail.com</div>
+
+          </div>
+          <div className="col-2">
+            <div className="title">about me</div>
+            <div className="infor">
+            Born and raised in Quang Phu town, Cumgar district, Dak Lak province. Since childhood, I have had a special interest in computers and technology.
+            </div>
+          </div>
+        </div>
+        <div className="line"></div>
+        <div className="row-3">
+          <div className="col-1">
+            <div className="title">education</div>
+            <div className="infor">Learned at Cumgar HighSchool</div>
+            <div className="infor">Studying at FPT Polytechnic</div>
+            <div className="infor"></div>
+
+          </div>
+          <div className="col-2">
+            <div className="title">experience</div>
+            <div className="infor">
+                
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Profile;
